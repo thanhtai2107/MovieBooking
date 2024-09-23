b284ea9e22b10e5b5267e82e5044d09e63429ec8
PhongDoan200299 <97325515+PhongDoan200299@users.noreply.github.com>
2024-09-23 13:23:14 +0700

    finish showtimes screen 3.1.5.1

--- MovieBooking 2/src/main/java/com/example/MovieBooking/service/IMovieService.java
+++ MovieBooking 2/src/main/java/com/example/MovieBooking/service/IMovieService.java
@@ -1,4 +1,10 @@
 package com.example.MovieBooking.service;
 
+import com.example.MovieBooking.entity.Movie;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface IMovieService {
+    public List<Movie> getMoviesByDate(LocalDate date);
 }
