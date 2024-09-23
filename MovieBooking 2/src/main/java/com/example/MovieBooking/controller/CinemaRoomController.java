bb35413a82136d985f4c2b5715ad023546a9ccce
vanbilly2001 <vanbilly01042001@gmail.com>
2024-09-23 16:37:37 +0700

    update code of Manage Cinema Room

--- MovieBooking 2/src/main/java/com/example/MovieBooking/controller/CinemaRoomController.java
+++ MovieBooking 2/src/main/java/com/example/MovieBooking/controller/CinemaRoomController.java
@@ -1,14 +1,20 @@
 package com.example.MovieBooking.controller;
 
 import com.example.MovieBooking.entity.CinemaRoom;
+import com.example.MovieBooking.entity.Seat;
+import com.example.MovieBooking.entity.SeatType;
 import com.example.MovieBooking.service.impl.CinemaRoomServiceImpl;
+import com.example.MovieBooking.service.impl.SeatServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -18,21 +24,78 @@ public class CinemaRoomController {
     @Autowired
     private CinemaRoomServiceImpl cinemaRoomService;
 
+    @Autowired
+    private SeatServiceImpl seatService;
+
+
     @GetMapping("/listCinemaRoom")
-    public String listCinemaRoom(Model model) {
-        List<CinemaRoom> listCinemaRoom = cinemaRoomService.getAllCinemaRooms();
+    public String listCinemaRoom(@RequestParam(value = "valueSearch", defaultValue = "", required = false) String valueSearch,
+                                 @RequestParam(value = "page", defaultValue = "0") int page,
+                                 @RequestParam(value = "size", defaultValue = "1") int size,
+                                 Model model) {
+        String search = "";
+        Page<CinemaRoom> listCinemaRoom;
+        if(valueSearch != null && !valueSearch.equals("")) {
+            search = valueSearch;
+            try{
+                Long valueSearhLong = Long.parseLong(search);
+                listCinemaRoom = cinemaRoomService.getCinemaRoomsByIdOrBySeatQuantity(valueSearhLong, page, size);
+            } catch(NumberFormatException e){
+                listCinemaRoom = cinemaRoomService.getCinemaRoomsByName(search, page, size);
+            }
+        } else {
+            listCinemaRoom = cinemaRoomService.getCinemaRoomsByName(search, page, size);
+        }
+
         model.addAttribute("listCinemaRoom", listCinemaRoom);
+        model.addAttribute("totalPages", listCinemaRoom.getTotalPages());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("size", size);
+        model.addAttribute("search", search);
+
         return "cinemaRoom/listCinemaRoom";
     }
 
-    @GetMapping("/addCinemaRoom")
-    public String formAddCinemaRoom(CinemaRoom cinemaRoom) {
+    @GetMapping("/formAddCinemaRoom")
+    public String formAddCinemaRoom(Model model) {
+        CinemaRoom cinemaRoom = new CinemaRoom();
+        model.addAttribute("cinemaRoom", cinemaRoom);
         return "cinemaRoom/addCinemaRoom";
     }
 
-    @PostMapping
-    public String addCinemaRoom(CinemaRoom cinemaRoom) {
-
+    @PostMapping("/addCinemaRoom")
+    public String addCinemaRoom(@ModelAttribute("cinemaRoom") CinemaRoom cinemaRoom) {
+        cinemaRoomService.saveCinemaRoom(cinemaRoom);
+        return "redirect:/cinemaRoom/listCinemaRoom";
     }
+
+    @GetMapping("/seatDetail")
+    public String seatDetailPage(@RequestParam("cinemaRoomId") Long cinemaRoomId, Model model) {
+        CinemaRoom cinemaRoom = cinemaRoomService.getCinemaRoomById(cinemaRoomId);
+        List<Seat> listSeat = seatService.listSeatByCinemaRoomId(cinemaRoomId);
+        // Tạo danh sách để chứa các hàng ghế
+        List<List<Seat>> rows = new ArrayList<>();
+
+        // Chia danh sách ghế thành các hàng
+        for (int i = 0; i < listSeat.size(); i += 6) {
+            int end = Math.min(i + 6, listSeat.size());
+            rows.add(listSeat.subList(i, end));
+        }
+
+        model.addAttribute("seatRows", rows); // Thêm danh sách hàng vào model
+        model.addAttribute("nameCinemaRoom", cinemaRoom.getCinemaName());
+        return "cinemaRoom/seatDetail";
+    }
+
+    //Cập nhật nhiều seat
+    @PostMapping("/updateSeatType")
+    public String updateSeatType(@RequestParam("selectedSeat") Long[] selectedSeat,@RequestParam("seatType") String seatType) {
+
+        // Gọi service để cập nhật loại ghế
+        seatService.updateListSeatType(selectedSeat, seatType);
+
+        return "redirect:/cinemaRoom/listCinemaRoom";
+    }
+
 
 }
